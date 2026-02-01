ad789711017fe85627861e980a7ec39112ed0ab5
Boss-tech <rehmanabdul78600786@gmail.com>
2026-02-01 13:27:02 +0500

    Update ytdl.js

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -47,7 +47,7 @@ cmd({
       views: _0x25a862,
       author: _0x2356d
     } = _0x3b9c50.results[0x0];
-    let _0x206abf = "αяѕℓαη-м∂ мυℓтιρℓє ρσωєяƒυℓ ωнαтѕαρρ вσт„\n\n" + ("*Title:* " + (_0x417755 || "Unknown") + "\n") + ("*Duration:* " + (_0x51f7d2 || 'Unknown') + "\n") + ("*Views:* " + (_0x25a862 || 'Unknown') + "\n") + ("*Release Ago:* " + (_0x3ea374 || 'Unknown') + "\n") + ("*Author:* " + (_0x2356d?.["name"] || 'Unknown') + "\n") + ("p*Url:* " + (_0x53641c || 'Unknown') + "\n\n") + ('' + (config.FOOTER || "ð“†©REDZONE-MD Official“†ª"));
+    let _0x206abf = "𝓑𝓞𝓢𝓢-𝓜𝓓 мυℓтιρℓє ρσωєяƒυℓ ωнαтѕαρρ вσт„\n\n" + ("*Title:* " + (_0x417755 || "Unknown") + "\n") + ("*Duration:* " + (_0x51f7d2 || 'Unknown') + "\n") + ("*Views:* " + (_0x25a862 || 'Unknown') + "\n") + ("*Release Ago:* " + (_0x3ea374 || 'Unknown') + "\n") + ("*Author:* " + (_0x2356d?.["name"] || 'Unknown') + "\n") + ("p*Url:* " + (_0x53641c || 'Unknown') + "\n\n") + ('' + (config.FOOTER || "ð“†𝐵𝒪𝒮𝒮-𝑀𝒟 Official“†ª"));
     const _0x2110e4 = await _0x4be93c.sendMessage(_0x2c7589, {
       'image': {
         'url': _0x434dfd
@@ -58,7 +58,7 @@ cmd({
     });
     await _0x4be93c.sendMessage(_0x2c7589, {
       'react': {
-        'text': 'REDZONE-MD',
+        'text': '𝘽𝙊𝙎𝙎-𝙈𝘿',
         'key': _0x2110e4.key
       }
     });
